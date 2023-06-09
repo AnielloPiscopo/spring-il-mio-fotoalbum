408c370a88ef4da93292d9c1502e6fda9c3efefd
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-06-09 10:15:34 +0200

    Created the repo

---
+++ backend-il-mio-foto-album/src/main/java/org/java/spring/BackendIlMioFotoAlbumApplication.java
@@ -0,0 +1,13 @@
+package org.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIlMioFotoAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIlMioFotoAlbumApplication.class, args);
+	}
+
+}
